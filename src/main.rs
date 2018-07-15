6ede6c1318079adb4e2e7fcc0561eb3e85d697be
Dmitry Safonov <dimanss47@dimanss47.net>
2018-07-15 11:16:50 +0000

    improve readability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![allow(unknown_lints)]
+#![warn(clippy)]
+
 extern crate capabilities;
 #[macro_use] extern crate clap;
 extern crate env_logger;
@@ -12,6 +15,7 @@ extern crate toml;
 use std::fs::*;
 use std::io::prelude::*;
 use std::mem::*;
+use std::path::PathBuf;
 use std::process::*;
 
 use capabilities::*;
@@ -37,7 +41,9 @@ fn DEFAULT_HDPARM_PATH() -> String {
 struct Config {
     devices: Vec<String>,
     #[serde(default = "DEFAULT_HDPARM_PATH")]
-    hdparm_path: String
+    hdparm_path: String,
+    #[serde(skip)]
+    command_arg: String
 }
 
 fn main() {
@@ -57,6 +63,48 @@ fn main() {
 fn the_main() -> Result<()> {
     env_logger::init();
 
+    let config = get_config()?;
+
+    if log_enabled!(Debug) {
+        debug!("read config:");
+        for i in toml::to_string(&config)
+                .context("error serializing configuration for logging")?
+                .lines() {
+            debug!("\t{}", i);
+        }
+    }
+
+    gain_caps()?;
+    debug!("set capabilities");
+
+    for g in &config.devices {
+        debug!("processing glob \"{}\"", g);
+        let files = glob_with(&g, &MatchOptions {
+            require_literal_separator: true,
+            require_literal_leading_dot: true,
+            ..MatchOptions::default()
+        }).context("error listing device files")?;
+
+        for i in files {
+            let dev_filename = match i {
+                Ok(x) => {
+                    debug!("found device file at {}", x.to_string_lossy());
+                    x
+                },
+                Err(e) => {
+                    eprintln!("failed to list file: {}", e);
+                    continue;
+                }
+            };
+
+            process_devfile(&config, &dev_filename)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn get_config() -> Result<Config> {
     let matches = App::new(crate_name!())
         .version(crate_version!())
         .author(crate_authors!())
@@ -74,17 +122,15 @@ fn the_main() -> Result<()> {
         .context("error reading from the configuration file")?;
     drop(config_file);
 
-    let config: Config = toml::from_str(&config_str)
+    let mut config: Config = toml::from_str(&config_str)
         .context("error parsing the configuration")?;
-    if log_enabled!(Debug) {
-        debug!("read config:");
-        for i in toml::to_string(&config)
-                .context("error serializing configuration for logging")?
-                .lines() {
-            debug!("\t{}", i);
-        }
-    }
 
+    config.command_arg = matches.value_of("loudness").unwrap().to_string();
+
+    Ok(config)
+}
+
+fn gain_caps() -> Result<()> {
     let mut caps = Capabilities::new()
         .context("error initializing capabilities")?;
 
@@ -103,49 +149,6 @@ fn the_main() -> Result<()> {
         .context("error setting ambient capabilities")?;
     set_ambient_cap(CAP_SYS_RAWIO)
         .context("error setting ambient capabilities")?;
-    debug!("set capabilities");
-
-    let command_arg = matches.value_of("loudness").unwrap();
-    let hdparm_arg = match command_arg {
-        "quiet" => "128",
-        "loud" => "254",
-        _ => ""
-    };
-
-    for g in config.devices {
-        debug!("processing glob \"{}\"", g);
-        let files = glob_with(&g, &MatchOptions {
-            require_literal_separator: true,
-            require_literal_leading_dot: true,
-            ..MatchOptions::default()
-        }).context("error listing device files")?;
-
-        for i in files {
-            let dev_filename = match i {
-                Ok(x) => {
-                    debug!("found device file at {}", x.to_string_lossy());
-                    x
-                },
-                Err(e) => {
-                    eprintln!("Failed to read filename (did a directory \
-                        match the pattern?): {}", e);
-                    continue;
-                }
-            };
-
-            let mut cmd = Command::new(&config.hdparm_path);
-            cmd.arg("-M");
-            if command_arg == "show" {
-                cmd.arg(&dev_filename);
-            } else {
-                cmd.arg(hdparm_arg).arg(&dev_filename);
-            };
-            cmd.spawn().context("error calling hdparm")?
-                .wait().context("error waiting for hdparm to complete")?;
-            debug!("executed hdparm for {}",
-                dev_filename.to_string_lossy());
-        }
-    }
 
     Ok(())
 }
@@ -154,9 +157,9 @@ fn set_ambient_cap(cap: u8) -> Result<()> { unsafe {
     let ret = prctl(
         PR_CAP_AMBIENT,
         PR_CAP_AMBIENT_RAISE as c_ulong,
-        cap as c_ulong,
-        0 as c_ulong,
-        0 as c_ulong
+        c_ulong::from(cap),
+        0,
+        0
     );
     if ret == -1 {
         bail!("unable to set ambient capabilities: {}",
@@ -164,3 +167,28 @@ fn set_ambient_cap(cap: u8) -> Result<()> { unsafe {
     }
     Ok(())
 }}
+
+fn process_devfile(config: &Config, dev_filename: &PathBuf) -> Result<()> {
+    let mut cmd = Command::new(&config.hdparm_path);
+        cmd.arg("-M");
+        if config.command_arg == "show" {
+            cmd.arg(&dev_filename);
+        } else {
+            let hdparm_arg = translate_arg(&config)?;
+            cmd.arg(hdparm_arg).arg(&dev_filename.as_os_str());
+        };
+        cmd.spawn().context("error calling hdparm")?
+            .wait().context("error waiting for hdparm to complete")?;
+    debug!("executed hdparm for {}",
+        dev_filename.to_string_lossy());
+
+    Ok(())
+}
+
+fn translate_arg(config: &Config) -> Result<String> {
+    Ok(match config.command_arg.as_str() {
+        "quiet" => String::from("128"),
+        "loud" => String::from("254"),
+        _ => bail!("wrong command argument")
+    })
+}
